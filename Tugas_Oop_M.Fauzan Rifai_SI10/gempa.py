16fcfa9a09e89f4bbb8d3613a0b2c7d7bd150bc3
Muhamadfauzan22 <114563070+Muhamadfauzan22@users.noreply.github.com>
2022-12-18 21:25:00 +0700

    Add files via upload

---
+++ Tugas_Oop_M.Fauzan Rifai_SI10/gempa.py
@@ -0,0 +1,26 @@
+class Gempa:
+    # member 1 variabel
+
+    lokasi = ""
+    skala = 0
+    
+    # member 2 konstrruktur
+    def __init__(self,lokasi,skala):
+        self.lokasi = lokasi
+        self.skala = skala
+        
+        # member 3 method
+    def dampak(self):
+        if(self.skala < 2):
+             ket ="tidak terasa"
+        elif(self.skala >=2 and self.skala <4):
+            ket = "Bangunan retak - retak"
+        elif(self.skala >=4 and self.skala <6):
+           ket = "Bangunan pada roboh"
+        else:
+                ket = "Bangunan roboh dan berpotensi tsunami"
+                print(
+                '\nlokasi gempa\t:',self.lokasi,
+                '\nskala\t\t:',self.skala,'richter'
+                '\ndampak\t\t:',ket,
+                '\n===============================')
